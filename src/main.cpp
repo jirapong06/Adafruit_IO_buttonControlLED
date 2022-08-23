2dd27a300df049e6335d70b64577af11d379892b
dmtt-admin <jirapong06@gmail.com>
2022-08-23 17:38:21 +0700

    Add DHT sensor

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,18 @@
 #define LED_PIN 13
 AdafruitIO_Feed *button = io.feed("controlBtn");
 
+
+#include "DHT.h"
+#define DHTPIN 18
+#define DHTTYPE DHT22
+DHT dht(DHTPIN, DHTTYPE);
+float tempSensor; 
+float humiditySensor;
+AdafruitIO_Feed *tempFeed = io.feed("temp");
+AdafruitIO_Feed *humFeed = io.feed("hum");
+
+unsigned long lastSendData = 0;
+
 void handleButton(AdafruitIO_Data *data) {
   Serial.println(data->toString());
   if (data->toString() == "ON") {
@@ -15,8 +27,9 @@ void handleButton(AdafruitIO_Data *data) {
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(LED_PIN, OUTPUT);
+  pinMode(LED_PIN, OUTPUT); 
   Serial.begin(115200);
+  dht.begin();
   Serial.print("Connecting to Adafruit IO");
   io.connect();
   button->onMessage(handleButton);
@@ -35,6 +48,17 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  if (millis() - lastSendData >= 1000*60) {
+    humiditySensor = dht.readHumidity();
+    tempSensor = dht.readTemperature();
+
+    humFeed->save(humiditySensor);
+    tempFeed->save(tempSensor);
+    
+    lastSendData = millis();
+  }
+
   io.run();
 
 }
